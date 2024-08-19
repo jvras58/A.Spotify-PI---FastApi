7b48f7fac084e62c48dca9a43cfbc5694336b595
jvras <48683351+jvras58@users.noreply.github.com>
2024-08-19 15:53:17 -0300

    Issue 02 add get artists info (#2)
    * feat: Add Spotify API get_artists_info router and schema
    
    * Refactor: refactor Spotify API artists_info endpoint and schema add save method post
    
    * feat: Fetch and save artists' em controller router
    
    * feat: Atualizar título do projeto no README.md
    
    ---------
    
    Co-authored-by: jvras <jonathas.vincius@ufpe.br>

---
+++ app/api/spotify/controller.py
@@ -0,0 +1,75 @@
+from http import HTTPStatus
+from typing import List
+
+import requests
+from fastapi import HTTPException
+from sqlalchemy import select
+
+from app.database.session import Session
+from app.models.artist import Artist
+from app.utils.base_model import AbstractBaseModel
+from app.utils.generic_controller import GenericController
+
+
+class ArtistController(GenericController):
+    def __init__(self) -> None:
+        super().__init__(Artist)
+
+    def get_artist_by_name(self, db_session: Session, name: str) -> Artist:
+        return db_session.scalar(select(Artist).where(Artist.name == name))
+
+    def fetch_artists_info(
+        self, spotify_access_token: str, artist_ids: List[str]
+    ) -> List[Artist]:
+        ids_str = ','.join(artist_ids)
+        url = f'https://api.spotify.com/v1/artists?ids={ids_str}'
+        headers = {'Authorization': f'Bearer {spotify_access_token}'}
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code != 200:
+            try:
+                detail = response.json()
+            except ValueError:
+                detail = response.text
+            raise HTTPException(status_code=response.status_code, detail=detail)
+
+        data = response.json()
+        artists_data = data.get('artists', [])
+
+        invalid_ids = [
+            artist_id
+            for artist_id, artist_data in zip(artist_ids, artists_data)
+            if artist_data is None
+        ]
+        if invalid_ids:
+            raise HTTPException(
+                status_code=HTTPStatus.NOT_FOUND,
+                detail=f'Um ou mais IDs de artista não encontrados: {", ".join(invalid_ids)}',
+            )
+
+        artists = [
+            Artist(
+                name=artist_data.get('name'),
+                followers=artist_data.get('followers', {}).get('total'),
+                popularity=artist_data.get('popularity'),
+            )
+            for artist_data in artists_data
+            if artist_data is not None
+        ]
+        return artists
+
+    def save(self, db_session: Session, obj: Artist) -> AbstractBaseModel:
+        existe_artist = self.get_artist_by_name(db_session, obj.name)
+        if existe_artist:
+            if (
+                existe_artist.followers != obj.followers
+                or existe_artist.popularity != obj.popularity
+            ):
+                existe_artist.followers = obj.followers
+                existe_artist.popularity = obj.popularity
+                db_session.commit()
+                return existe_artist
+            else:
+                return existe_artist
+        return super().save(db_session, obj)
--- app/api/spotify/router.py
+++ app/api/spotify/router.py
@@ -1,26 +1,34 @@
 from http import HTTPStatus
-from typing import Annotated
+from typing import Annotated, List
 
 import requests
 from cachetools import TTLCache, cached
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query, Request
 from sqlalchemy.orm import Session
 
 from app.api.authentication.controller import get_current_user
-from app.api.spotify.schemas import SpotifyType
+from app.api.spotify.controller import ArtistController
+from app.api.spotify.schemas import ArtistInfoSchema, SpotifyType
 from app.database.session import get_session
 from app.models.user import User
+from app.utils.exceptions import IntegrityValidationException
 
 router = APIRouter()
+artist_controller = ArtistController()
 
 db_session_type = Annotated[Session, Depends(get_session)]
 CurrentUser = Annotated[User, Depends(get_current_user)]
+
 
 token_cache = TTLCache(maxsize=1, ttl=3600)
 
 
 @cached(cache=token_cache)
-@router.get('/{spotify_type}/{spotify_search}')
+@router.get(
+    '/{spotify_type}/{spotify_search}',
+    summary='Obter informações do catálogo do Spotify',
+    response_model=dict,
+)
 def search_spotify_data_by_type(
     spotify_type: SpotifyType,
     spotify_search: str,
@@ -46,3 +54,47 @@ def search_spotify_data_by_type(
         raise HTTPException(status_code=response.status_code, detail=response.json())
 
     return response.json()
+
+
+@router.post('/artists', response_model=List[ArtistInfoSchema])
+def artists_info(
+    current_user: CurrentUser,
+    db_session: db_session_type,
+    request: Request,
+    artist_ids: List[str] = Query(
+        ..., description='Lista de IDs de artistas no Spotify'
+    ),
+) -> List[ArtistInfoSchema]:
+    """
+    Cria novos artistas no banco de dados a partir de uma lista de IDs de artistas do Spotify.
+    """
+    spotify_access_token = getattr(current_user, 'spotify_access_token', None)
+    if not spotify_access_token:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail='Token do Spotify não encontrado.',
+        )
+
+    try:
+        artists = artist_controller.fetch_artists_info(spotify_access_token, artist_ids)
+    except HTTPException as ex:
+        raise HTTPException(
+            status_code=ex.status_code,
+            detail=ex.detail,
+        ) from ex
+
+    created_artists = []
+    for artist in artists:
+        artist.audit_user_ip = request.client.host
+        artist.audit_user_login = current_user.username
+
+        try:
+            saved_artist = artist_controller.save(db_session, artist)
+            created_artists.append(saved_artist)
+        except IntegrityValidationException as ex:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Object Artist was not accepted',
+            ) from ex
+
+    return created_artists
